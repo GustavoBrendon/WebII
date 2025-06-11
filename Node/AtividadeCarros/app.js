885b7ed91cabd147d6e8ad48332f5e1067c43153
Gustavo Brendon <guhgomesyt@gmail.com>
2025-06-11 10:09:15 -0300

    Atividade 08 - Finalizada

--- Node/AtividadeCarros/app.js
+++ Node/AtividadeCarros/app.js
@@ -3,6 +3,9 @@ import syncer from './database/syncer.js';
 import film_router from './routers/carro_router.js';
 import actor_router from './routers/motorista_router.js';
 import director_router from './routers/acessorio_router.js';
+import acessorio_router from './routers/acessorio_router.js';
+import motorista_router from './routers/motorista_router.js';
+import carro_router from './routers/carro_router.js';
 
 if(!syncer()){
     process.exit();
@@ -15,9 +18,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.end('Rodando');
 });
-app.use('/carros', film_router);
-app.use('/acessorios', actor_router);
-app.use('/motoristas', director_router);
+app.use('/carros', carro_router);
+app.use('/acessorios', acessorio_router);
+app.use('/motoristas', motorista_router);
 
 app.listen(80, () => {
     console.log('Escutando...');
--- Node/AtividadeCarros/models/acessorio.js
+++ Node/AtividadeCarros/models/acessorio.js
@@ -3,7 +3,7 @@ import sequelize from "../database/mysql.js";
 
 const Acessorio = sequelize.define('Acessorio', {
     nome: DataTypes.STRING,
-    utilidade: DataTypes.DATEONLY    
+    utilidade: DataTypes.STRING    
 });
 
 export default Acessorio;
