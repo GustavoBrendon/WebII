02ecd3fc84579557ce9832a92a8d1176a9b0b380
GustavoBrendon <guhgomesyt@gmail.com>
2025-07-02 20:33:01 -0300

    Render

--- Node/AtividadeCarros/database/mysql.js
+++ Node/AtividadeCarros/database/mysql.js
@@ -1,12 +1,16 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize({
-    dialect: 'mysql',
-    host: 'localhost',
-    port: '3306',
-    username: 'root',
-    password: 'root',
-    database: 'atividade_node'
-});
+// const sequelize = new Sequelize({
+//     dialect: 'mysql',
+//     host: 'localhost',
+//     port: '3306',
+//     username: 'root',
+//     password: 'root',
+//     database: 'atividade_node'
+// });
+
+const sequelize = new Sequelize('postgresql://projeto_carros_user:zPzmM99Y8lfMKvUPzAN37AK99Nr0JRVh@dpg-d1is273ipnbc7382v310-a/projeto_carros_db');
+
+
 
 export default sequelize;
