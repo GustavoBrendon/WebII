21d94fe02d2689ef14a20c3ed627be79f5e9bd8f
GustavoBrendon <guhgomesyt@gmail.com>
2025-07-02 10:34:14 -0300

    Inicializando react

---
+++ Node/pratica-react/src/components/list.jsx
@@ -0,0 +1,27 @@
+import styles from './list.module.css';
+
+const List = (props) => {
+
+    return (
+
+        <ul className={styles.ul}>
+
+            {
+
+                props.items.map((v, i) => {
+
+                    return <li className={styles.li}>{v}</li>;
+
+                })
+
+            }
+
+        </ul>
+
+    );
+
+};
+
+
+
+export default List;
